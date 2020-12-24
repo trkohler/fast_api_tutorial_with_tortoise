d915f5286a90d9c88add61d9a0c63c961bf62389
trkohler <troyalekson@gmail.com>
2020-12-24 20:49:24 +0200

    async tests

--- test_app.py
+++ test_app.py
@@ -1,7 +1,23 @@
+import asyncio
+from tortoise_models import SoftwareEngineers
+from typing import Generator
+import pytest
 from fastapi.testclient import TestClient
-from .main import app
+from main import app
+from tortoise.contrib.test import finalizer, initializer
 
-client = TestClient(app)
+
+@pytest.fixture(scope="module")
+def client() -> Generator:
+    initializer(["tortoise_models"])
+    with TestClient(app) as c:
+        yield c
+    finalizer()
+
+
+@pytest.fixture(scope="module")
+def event_loop(client: TestClient) -> Generator:
+    yield client.task.get_loop()
 
 
 def test_read_main():
@@ -28,20 +44,25 @@ def test_dynamic_routing_with_query_params():
     assert response.json() == {"number": (number + add) * multiply}
 
 
-def test_create_engineer():
+def test_create_engineer(client: TestClient, event_loop: asyncio.AbstractEventLoop):
     main_language: str = "Python"
     years_experience: int = 2
     likes_coffee: bool = True
     password: str = "mysupersecretpassword"
-
     payload = {
         "main_language": main_language,
         "years_experience": years_experience,
-        "likes_coffee": likes_coffee,
+        "loves_coffee": likes_coffee,
         "password": password,
     }
-
     response = client.post("/software_engineers/", json=payload)
+    data = response.json()
     assert response.status_code == 200
-    # assert response.json() == payload
-    assert "password" not in response.json().keys()  # new !
+    assert "password_hash" not in data.keys()
+
+    async def get_engineer_by_db():
+        engineer = await SoftwareEngineers.get(uuid=data.get("uuid"))
+        return engineer
+
+    eng_obj = event_loop.run_until_complete(get_engineer_by_db())
+    assert str(eng_obj.uuid) == data.get("uuid")
