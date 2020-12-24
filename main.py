21dee8fbe8b8c6fe0a995f2483d894a0eb3301d9
trkohler <troyalekson@gmail.com>
2020-12-24 17:42:30 +0200

    dynamic routing: part 1

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "hello world"}
+
+
+@app.get("/dynamic_routing/{number}")
+async def return_number(number: int):  # type annotation syntax
+    return {"number": number}
